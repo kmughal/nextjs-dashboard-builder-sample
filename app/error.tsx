a7a202753934f5f4c481a70ef154180730b1fb6d
K.H.U.R.R.A.M. <kmughal@gmail.com>
2024-05-26 11:53:31 +0100

    chore: added a basic error page

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+'use client';
+export default function ErrorPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-gray-800">Error</h1>
+            <p className="text-gray-500">Something went wrong</p>
+        </div>
+    );
+}
