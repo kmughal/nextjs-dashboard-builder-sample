8b5418bd066ddbcaac66fbe807c7ce2e5d8f4b7a
K.H.U.R.R.A.M. <kmughal@gmail.com>
2024-05-26 11:49:50 +0100

    chore: just added a basic loading markup!

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+export default function LoadingPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold text-gray-800">Loading</h1>
+            <p className="text-gray-500">Please wait...</p>
+        </div>
+    );
+}
